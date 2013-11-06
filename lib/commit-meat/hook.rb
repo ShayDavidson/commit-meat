060c1d60cbc5bb77da5c95fd9e6e06ab5471ad6a
Shay Davidson <shay.h.davidson@gmail.com>
2013-11-06 10:43:18 +0200

    test

--- lib/commit-meat/hook.rb
+++ lib/commit-meat/hook.rb
@@ -8,11 +8,13 @@ rescue LoadError
 end
 
 def assert_with_message(condition, msg)
-  (puts msg.yellow) and (exit 1) unless condition
+  unless condition
+    puts msg.yellow
+    exit 1
+  end
 end
 
 message_file = ARGV[0]
 message = File.read(message_file).strip
 
-puts message.split.size > 1
 assert_with_message('Single word commit messages are not allowed.', message.split.size > 1)
