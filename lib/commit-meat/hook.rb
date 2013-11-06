f8507fe6ecf751c47506df5238b227165e0724e5
Shay Davidson <shay.h.davidson@gmail.com>
2013-11-06 08:15:21 +0200

    heyo

--- lib/commit-meat/hook.rb
+++ lib/commit-meat/hook.rb
@@ -2,15 +2,15 @@ module CommitMeat
 
   COMMIT_MESSAGE_HOOK = <<END
 
-#!/usr/bin/env ruby
+#!/usr/bin/env sh
 
-message_file = ARGV[0]
-message = File.read(message_file)
-puts message.green
-if message.length == 1
-  puts "Your commit message cannot be a single word".red
-  exit 1
-end
+echo "This is bash!"
+
+/usr/bin/env ruby <<-EORUBY
+
+  puts 'This is ruby!'
+
+EORUBY
 
 END
 
